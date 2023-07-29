39d08931e404bb7e82755f379141833016c3a88f
ZeyadAttia5 <85710632+ZeyadAttia5@users.noreply.github.com>
2023-07-29 22:37:58 +0300

    added a semaphore for the chairs
    bug: student threads exit prematurly

--- helpdesk.c
+++ helpdesk.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <pthread.h>
+#include <stdlib.h>
 #include <time.h>
 #include <inttypes.h>
 #include <semaphore.h>
@@ -58,20 +59,25 @@ pthread_cond_t cond_student;
 pthread_cond_t is_student;
 pthread_mutex_t lock_chairs;
 
+pthread_mutex_t mstudent_lock;
+
 pthread_mutex_t mlock_print;
+
+sem_t chair_semaphore;
+
+uint8_t ta_exit = 0;
+int students_helped = 0; // New counter to track the number of students helped
 
 int main()
 {
 
+    sem_init(&chair_semaphore, 0, CHAIR_NUM);
     pthread_mutex_init(&lock_chairs, NULL);
     pthread_mutex_init(&mlock_print, NULL);
     pthread_cond_init(&is_student, NULL);
     pthread_cond_init(&cond_student, NULL);
 
-    // for (int8_t i = 0; i < CHAIR_NUM; i++)
-    // {
-    //     printf("Student %i status is %i\n", chairs[i].student->status);
-    // }
+    srand((unsigned int)time(NULL));
 
     // create ta
     pthread_t ta;
@@ -81,24 +87,32 @@ int main()
     teacher.tid = ta;
     teacher.status = SLEEPING;
     pthread_create(&teacher.tid, &ta_attr, ta_init, &teacher);
+    Student all_students[STUDENT_NUM];
 
-    pthread_attr_t student_attr;
     for (size_t i = 0; i < STUDENT_NUM; i++)
     {
+        pthread_attr_t student_attr;
         pthread_t student_id;
         pthread_attr_init(&student_attr);
         Student s;
         s.number = i;
         s.student_id = student_id;
         s.status = PROGRAMMING;
+        all_students[i] = s;
         pthread_create(&s.student_id, &student_attr, student_init, &s);
     }
+    for (size_t i = 0; i < STUDENT_NUM; i++)
+    {
+        pthread_join(all_students[i].student_id, NULL);
+        printf("Student %i exited\n", all_students[i].number);
+    }
+    ta_exit = 1;
     pthread_join(teacher.tid, NULL);
 }
 
 void *ta_init(void *teachr)
 {
-    TA *teacher = teachr;
+    TA *teacher = (TA *)teachr;
     while (1)
     {
         while (chairs[0].status == UNOCCUPIED)
@@ -106,73 +120,84 @@ void *ta_init(void *teachr)
             teacher->status = SLEEPING;
             pthread_cond_wait(&is_student, &lock_chairs); // wait until a student comes and signals
         }
-        teacher->status = HELPING;
-        pthread_mutex_unlock(&lock_chairs);
-        sleep(1); // help the Student
 
+        Student *s = chairs[0].student;
+        pthread_mutex_unlock(&lock_chairs); // was lockedfrom the sit()
+
+        teacher->status = HELPING;
+        int random_number = rand() % 3 + 1;
+        sleep(random_number);             // help the Student
+        pthread_mutex_lock(&lock_chairs); // released in the student_init
         // signal thread to exit
-        pthread_cond_signal(&is_student);
-        // empty his chair
-        printf("Student %i is leaving\n", chairs[0].student->number);
-        vacate_chair(0);
-        pthread_mutex_unlock(&lock_chairs);
-        for (int8_t i = 0; i < CHAIR_NUM; i++)
+        pthread_mutex_lock(&mlock_exit); // unlocked in student_init()
+        exit_flag = s->number;
+        teacher->status = SLEEPING;
+        students_helped++; // Increment the students_helped counter
+        pthread_cond_signal(&cond_student);
+        // pthread_join(chairs[0].student->student_id, NULL);  //wait for the student to leave
+        if (ta_exit == 1 && students_helped == STUDENT_NUM)
         {
-            printf("Chair %i has student %i\n", i, chairs[i].student->number);
+            break;
         }
     }
+    return NULL;
 }
 
 void *student_init(void *stu)
 {
-    Student *s = stu;
-    // if there are any chairs empty, sit
-
-    int8_t come_back = 0;
-    // pthread_mutex_lock(&mlock_print);
-    // for (int8_t i = 0; i < CHAIR_NUM; i++)
-    // {
-    //     printf("Student %i is now sitting on chair %i\n", s->number, i);
-    // }
-    // pthread_mutex_unlock(&mlock_print);
+    Student *s = (Student *)stu;
     do
     {
-        if (sit(s) != -1)
+        sem_wait(&chair_semaphore);
+        int8_t chair_id = sit(s);
+        if (chair_id == -1)
         {
-            come_back = 0; // do not come back
+            // come back later
+            s->status = PROGRAMMING;
+            sem_post(&chair_semaphore);
+            pthread_mutex_lock(&mlock_print);
+            printf("student %i is programming\n", s->number);
+            pthread_mutex_unlock(&mlock_print);
+            int random_number = rand() % 3 + 1;
+            sleep(random_number);
         }
         else
         {
-            come_back = 1;
-            s->status = PROGRAMMING;
-            printf("student %i will go program\n", s->number);
-            sleep(4); // come back later;
-        }
-    } while (come_back == 1);
+            // TODO DELETE
+            pthread_mutex_lock(&mlock_print);
+            printf("Student %i is sitting\n", s->number);
+            pthread_mutex_unlock(&mlock_print);
 
-    while (s->number != exit_flag) // while the number was not signaled, wait
+            break; // the student is now sitting
+        }
+
+    } while (1);
+
+    pthread_mutex_lock(&lock_chairs); // released in ta_init
+    pthread_cond_signal(&is_student); // wake up the teacher
+    while (s->number != exit_flag)    // while the number was not signaled, wait
     {
-        pthread_cond_wait(&is_student, &lock_chairs); // wait until a student comes and signals
+        pthread_cond_wait(&cond_student, &lock_chairs); // wait until a signal comes to exit
     }
-    pthread_exit(NULL);
+    vacate_chair(0);
+    pthread_mutex_unlock(&lock_chairs); // lock was acquired by TA_init
+    pthread_mutex_unlock(&mlock_exit);  // lock was acquired by TA_init
+    return NULL;
 }
 
 // vacate the first chair and shift all students one unit to the left
 void vacate_chair(int8_t chair_id)
 {
     // signal thread to exit
-    pthread_mutex_lock(&mlock_exit);
-    exit_flag = chairs[0].student->number;
-    pthread_cond_signal(&cond_student);
-    pthread_mutex_unlock(&mlock_exit);
 
-    pthread_join(chairs[chair_id].student->student_id, NULL);
+    chairs[chair_id].status = UNOCCUPIED;
+    chairs[chair_id].student->status = UNINITIALIZED;
+    sem_post(&chair_semaphore);
 
-    pthread_mutex_lock(&lock_chairs);
-    chairs[chair_id].status = UNINITIALIZED;
+    // move next student to chairs[0] ensuring fairness --FIFO
     for (size_t i = 1; i < CHAIR_NUM; i++)
     {
-        if (chairs[i].status != UNINITIALIZED && chairs[i - 1].status != UNINITIALIZED) // two consecutive uninitialized students means that there are no more left
+        if (chairs[i].status != UNOCCUPIED && chairs[i - 1].status != UNOCCUPIED) // two consecutive uninitialized students means that there are no more left
         {
             chairs[i - 1] = chairs[i];
         }
@@ -192,6 +217,7 @@ int8_t last_empty_chair()
         if (chairs[i].status == UNOCCUPIED)
         {
             retVal = i; // chair is empty
+            break;
         }
     }
     return retVal;
@@ -200,17 +226,15 @@ int8_t last_empty_chair()
 int8_t sit(Student *s)
 {
     // use mutex or semaphore lock to edit the chairs array
-    pthread_mutex_lock(&lock_chairs);
+    pthread_mutex_lock(&lock_chairs); // released in the ta_init
     int8_t chair_id = last_empty_chair();
-    chairs[chair_id].student = s;
-    s->status = SITTING;
-    chairs[chair_id].status = OCCUPIED;
-    // printf("%i\n", chair_id);
-    pthread_mutex_lock(&mlock_print);
-    // printf("Student %i is sitting on chair %i\n", s->number, chair_id);
-    pthread_mutex_unlock(&mlock_print);
 
-    pthread_cond_signal(&is_student);
+    if (chair_id != -1)
+    {
+        chairs[chair_id].student = s;
+        s->status = SITTING;
+        chairs[chair_id].status = OCCUPIED;
+    }
     pthread_mutex_unlock(&lock_chairs);
     return chair_id; // indicating success
 }
